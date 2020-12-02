a98b3ba159d0d95c726c5f1a8e9e9a9d88aad52c
Ed <tardisman5197@users.noreply.github.com>
2020-12-02 21:39:50 +0000

    Implemented Day 1 Part 1
    * Solve part 1 of day one
    * Created utils package
    
    This package contains generic errors and functions
    that can read in inputs.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,7 +1,17 @@
+// Declare the modules in the package
 mod part1;
 
-fn main() {
-    println!("{}",part1::solve());
+extern crate utils;
+
+// main runs each part of the day's challenges
+fn main() -> Result<(),  Box<dyn std::error::Error>> {
+    // Read in the input
+    let lines: Vec<String> = utils::read_file_lines("./inputs/day1.txt".to_string())?;
+
+    // Run and print the result
+    println!("Day 1:\n\tPart 1: {}\n",part1::solve(lines)?);
+
+    Ok(())
 }
 
 
---
+++ utils/src/errors.rs
@@ -0,0 +1,36 @@
+use std::error::Error;
+use std::fmt;
+
+// NoResultError is a generic error
+// which shows that the answer could not
+// be found.
+#[derive(Debug)]
+pub struct NoResultError {
+    details: String
+}
+
+impl NoResultError {
+    // new creates an instance of the NoResultError,
+    // a description of the error can be passed in
+    // as a parameter.
+    pub fn new(msg: &str) -> NoResultError {
+        NoResultError{details: msg.to_string()}
+    }
+}
+
+// Implement fmt::Display, this needs to be done
+// to allow for the error to be displayed.
+impl fmt::Display for NoResultError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.details)
+    }
+}
+
+// Implement Error, this needs to be done
+// to allow NoResultError to be used as
+// a type Error.
+impl Error for NoResultError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
