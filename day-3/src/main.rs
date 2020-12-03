1edbecb618c280f9dbae7f09afa4ca72d7a27f90
Ed <tardisman5197@users.noreply.github.com>
2020-12-03 19:35:11 +0000

    Completed Day 3 Part 1

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,18 +1,16 @@
 // Declare the modules in the package
 mod part1;
-mod part2;
 
 extern crate utils;
 
 // main runs each part of the day's challenges
 fn main() -> Result<(),  Box<dyn std::error::Error>> {
     // Read in the input
-    let lines: Vec<String> = utils::read_file_lines("./inputs/day2.txt".to_string())?;
+    let lines: Vec<String> = utils::read_file_lines("./inputs/day3.txt".to_string())?;
 
     // Run and print the result
-    println!("Day 2:");
+    println!("Day 3:");
     println!("\tPart 1: {}", part1::solve(&lines)?);
-    println!("\tPart 2: {}", part2::solve(&lines)?);
 
     Ok(())
 }
