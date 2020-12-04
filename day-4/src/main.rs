205a9895163ed8b11ad114e8bf17028a4670b772
Ed <tardisman5197@users.noreply.github.com>
2020-12-04 10:41:32 +0000

    Init Day 4

---
+++ day-4/src/main.rs
@@ -0,0 +1,22 @@
+// Declare the modules in the package
+mod part1;
+
+extern crate utils;
+
+use std::time::{Instant};
+
+// main runs each part of the day's challenges
+fn main() -> Result<(),  Box<dyn std::error::Error>> {
+    // Read in the input
+    let lines: Vec<String> = utils::read_file_lines("./inputs/day4.txt".to_string())?;
+
+    // Run the solutions
+    let start = Instant::now();
+    let part1_solution = part1::solve(&lines)?;
+    let part1_duration = start.elapsed();
+
+    println!("Day 4:");
+    println!("\tPart 1: {} ({:?})", part1_solution, part1_duration);
+
+    Ok(())
+}
