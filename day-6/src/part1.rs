315f760da63c4906eaf061516ae04cdf6abcc393
Ed <tardisman5197@users.noreply.github.com>
2020-12-06 17:09:40 +0000

    Completed Day6 Part 1

--- day-6/src/part1.rs
+++ day-6/src/part1.rs
@@ -1,13 +1,35 @@
 extern crate utils;
+use std::collections::HashSet;
 
-// solve attempts to solve part 1 of day 5 of AoC 2020.
-// Go through each boarding pass and calculate the
-// seat id, then keep track of the highest seat id
-// seen.
-pub fn solve(lines: &Vec<String>) -> Result<u16, Box<dyn std::error::Error>> {
-    Err(Box::new(utils::errors::SomethingIsWrongError::new(
-        "Not Implemented",
-    )))
+// solve attempts to solve part 1 of day 6 of AoC 2020.
+// Calculate the sum of the unique values for each set
+// of declaration forms.
+pub fn solve(lines: &Vec<String>) -> Result<usize, Box<dyn std::error::Error>> {
+    let mut result: usize = 0;
+    let mut declaration_forms_answers: HashSet<char> = HashSet::new();
+
+    for line in lines.iter() {
+        // Check if there is a new set of
+        // declaration forms.
+        if line == "" {
+            result += declaration_forms_answers.len();
+            declaration_forms_answers = HashSet::new();
+            continue;
+        }
+
+        // Add all of the unique answers to the
+        // declaration forms answers.
+        for answer in line.chars() {
+            if !declaration_forms_answers.contains(&answer) {
+                declaration_forms_answers.insert(answer);
+            }
+        }
+    }
+
+    // Add the last set of forms
+    result += declaration_forms_answers.len();
+
+    Ok(result)
 }
 
 #[cfg(test)]
