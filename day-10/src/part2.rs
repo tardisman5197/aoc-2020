a257952edf6ac9eb33a9766d9ca0c12417a75915
Ed <tardisman5197@users.noreply.github.com>
2020-12-10 16:50:05 +0000

    Completed Day 10 Part 1

--- day-10/src/part2.rs
+++ day-10/src/part2.rs
@@ -2,21 +2,19 @@ extern crate utils;
 
 // solve attempts to solve part 1 of day 10 of AoC 2020.
 pub fn solve(lines: &Vec<String>) -> Result<i64, Box<dyn std::error::Error>> {
-    Err(Box::new(utils::errors::SomethingIsWrongError::new(
-        "Not Implemented",
-    )))
+    Ok(0)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_solve() -> Result<(), Box<dyn std::error::Error>> {
-        let file: &str = "";
-        let input: Vec<String> = file.lines().map(String::from).collect();
+    // #[test]
+    // fn test_solve() -> Result<(), Box<dyn std::error::Error>> {
+    //     let file: &str = "";
+    //     let input: Vec<String> = file.lines().map(String::from).collect();
 
-        assert_eq!(solve(&input)?, 0);
-        Ok(())
-    }
+    //     assert_eq!(solve(&input)?, 0);
+    //     Ok(())
+    // }
 }
