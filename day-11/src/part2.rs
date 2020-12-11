8fefacbdb435515e6b3127afb58f82d74130019b
Ed <tardisman5197@users.noreply.github.com>
2020-12-11 15:46:05 +0000

    Init Day 11

---
+++ day-11/src/part2.rs
@@ -0,0 +1,20 @@
+extern crate utils;
+
+// solve attempts to solve part 2 of day x of AoC 2020.
+pub fn solve(lines: &Vec<String>) -> Result<i64, Box<dyn std::error::Error>> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve() -> Result<(), Box<dyn std::error::Error>> {
+        let file: &str = "";
+        let input: Vec<String> = file.lines().map(String::from).collect();
+
+        assert_eq!(solve(&input)?, 0);
+        Ok(())
+    }
+}
