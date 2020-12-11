ca1fb6bfd13a2174c65985b9a85249698259e3d8
Ed <tardisman5197@users.noreply.github.com>
2020-12-11 23:51:00 +0000

    Optimisations

--- day-11/src/shared.rs
+++ day-11/src/shared.rs
@@ -1,46 +1,65 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::convert::TryFrom;
 
 // get_seats returns a set off all of the coordinates
-//pf seats in the input.
-fn get_seats(lines: &Vec<String>) -> Result<HashSet<(i64, i64)>, Box<dyn std::error::Error>> {
-    let mut seats: HashSet<(i64, i64)> = HashSet::new();
-
+// seats in the input.
+fn get_seats(
+    lines: &Vec<String>,
+    size: &(i64, i64),
+    directly_adjacent: bool,
+) -> Result<HashMap<(i64, i64), Vec<(i64, i64)>>, Box<dyn std::error::Error>> {
+    let mut seat_set: HashSet<(i64, i64)> = HashSet::new();
     for (y, line) in lines.iter().enumerate() {
         for (x, character) in line.chars().enumerate() {
             if character == 'L' {
-                seats.insert((i64::try_from(x)?, i64::try_from(y)?));
+                seat_set.insert((i64::try_from(x)?, i64::try_from(y)?));
             }
         }
     }
+    let mut seats: HashMap<(i64, i64), Vec<(i64, i64)>> = HashMap::new();
+    for seat in seat_set.iter() {
+        if directly_adjacent {
+            seats.insert(*seat, get_adjacent_neighbors(seat, &seat_set));
+        } else {
+            seats.insert(*seat, get_neighbors(seat, &seat_set, size));
+        }
+    }
+
     Ok(seats)
 }
 
-// directly_adjacent_occupied returns the number of occupied
-// tiles around the seat specified.
-fn directly_adjacent_occupied(seat: &(i64, i64), occupied: &HashSet<(i64, i64)>) -> i64 {
-    let mut occupied_adjacent: i64 = 0;
-    for x in 0..3 {
-        for y in 0..3 {
-            if x != 1 || y != 1 {
-                if occupied.contains(&(seat.0 + (x - 1), seat.1 + (y - 1))) {
-                    occupied_adjacent += 1;
+// get_adjacent_neighbors generates a Vec of the adjacent
+// seats to the seat specified.
+fn get_adjacent_neighbors(seat: &(i64, i64), seats: &HashSet<(i64, i64)>) -> Vec<(i64, i64)> {
+    let mut neighbors: Vec<(i64, i64)> = Vec::new();
+    // For each direction:
+    //  0 = negative
+    //  1 = equal
+    //  2 = positive
+    for i in 0..3 {
+        for j in 0..3 {
+            if i != 1 || j != 1 {
+                let position: (i64, i64) = (seat.0 + (i - 1), seat.1 + (j - 1));
+
+                if seats.contains(&position) {
+                    neighbors.push(position)
                 }
             }
         }
     }
-    return occupied_adjacent;
+
+    neighbors
 }
 
-// adjacent_occupied returns the number of occupied
-// seats around the seat specified.
-fn adjacent_occupied(
+// get_neighbors returns a Vec if the closest seats
+// in every direction to the seat specified.
+fn get_neighbors(
     seat: &(i64, i64),
-    occupied: &HashSet<(i64, i64)>,
     seats: &HashSet<(i64, i64)>,
     size: &(i64, i64),
-) -> i64 {
-    let mut occupied_adjacent: i64 = 0;
+) -> Vec<(i64, i64)> {
+    let mut neighbors: Vec<(i64, i64)> = Vec::new();
     // For each direction:
     //  0 = negative
     //  1 = equal
@@ -50,25 +69,44 @@ fn adjacent_occupied(
             if dir_x != 1 || dir_y != 1 {
                 let delta_x: i64 = dir_x - 1;
                 let delta_y: i64 = dir_y - 1;
-
                 let mut x: i64 = seat.0;
                 let mut y: i64 = seat.1;
-
                 while (x >= 0 && x < size.0) && (y >= 0 && y < size.1) {
                     x += delta_x;
                     y += delta_y;
-
                     if seats.contains(&(x, y)) {
-                        if occupied.contains(&(x, y)) {
-                            occupied_adjacent += 1;
-                        }
+                        neighbors.push((x, y));
                         break;
                     }
                 }
             }
         }
     }
-    return occupied_adjacent;
+
+    neighbors
+}
+
+// get_occupied_neighbors returns the number of
+// occupied seats neighboring the specified seat.
+fn get_occupied_neighbors(
+    seat: &(i64, i64),
+    occupied: &HashSet<(i64, i64)>,
+    seats: &HashMap<(i64, i64), Vec<(i64, i64)>>,
+) -> i64 {
+    let mut occupied_neighbors: i64 = 0;
+
+    let neighbors: &Vec<(i64, i64)> = match seats.get(seat) {
+        Some(neighbors) => neighbors,
+        None => return 0,
+    };
+
+    for neighbor in neighbors.iter() {
+        if occupied.contains(neighbor) {
+            occupied_neighbors += 1;
+        }
+    }
+
+    occupied_neighbors
 }
 
 // update_seat returns true if the seat
@@ -76,29 +114,23 @@ fn adjacent_occupied(
 fn update_seat(
     seat: &(i64, i64),
     occupied: &HashSet<(i64, i64)>,
-    seats: &HashSet<(i64, i64)>,
-    size: &(i64, i64),
+    seats: &HashMap<(i64, i64), Vec<(i64, i64)>>,
     directly_adjacent: bool,
 ) -> bool {
-    let ao: i64;
-    if directly_adjacent {
-        ao = directly_adjacent_occupied(seat, occupied);
-    } else {
-        ao = adjacent_occupied(seat, occupied, seats, size);
-    }
+    let occupied_neighbors = get_occupied_neighbors(seat, occupied, seats);
     if occupied.contains(seat) {
         if directly_adjacent {
-            if ao >= 4 {
+            if occupied_neighbors >= 4 {
                 return false;
             }
         } else {
-            if ao >= 5 {
+            if occupied_neighbors >= 5 {
                 return false;
             }
         }
         return true;
     }
-    if ao == 0 {
+    if occupied_neighbors == 0 {
         return true;
     }
     return false;
@@ -121,15 +153,15 @@ pub fn occupied_seats_in_steady_state(
         }
     };
     let size: (i64, i64) = (x_size, y_size);
-    let seats: HashSet<(i64, i64)> = get_seats(lines)?;
+    let seats: HashMap<(i64, i64), Vec<(i64, i64)>> = get_seats(lines, &size, directly_adjacent)?;
 
     let mut previous_occupied: HashSet<(i64, i64)> = HashSet::new();
 
     loop {
         let mut occupied: HashSet<(i64, i64)> = HashSet::new();
 
-        for seat in seats.iter() {
-            if update_seat(&seat, &previous_occupied, &seats, &size, directly_adjacent) {
+        for seat in seats.keys() {
+            if update_seat(&seat, &previous_occupied, &seats, directly_adjacent) {
                 occupied.insert(*seat);
             }
         }
@@ -142,66 +174,4 @@ pub fn occupied_seats_in_steady_state(
     }
 
     Ok(previous_occupied.len())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_adjacent_occupied() -> Result<(), Box<dyn std::error::Error>> {
-        // #L.
-        // .#.
-        // ...
-        let mut seats: HashSet<(i64, i64)> = HashSet::new();
-        seats.insert((0, 1));
-        seats.insert((0, 0));
-        seats.insert((1, 1));
-        let mut occupied: HashSet<(i64, i64)> = HashSet::new();
-        occupied.insert((0, 0));
-        assert_eq!(adjacent_occupied(&(1, 1), &seats, &occupied, &(3, 3)), 1);
-
-        // ##.
-        // .#.
-        // ...
-        let mut seats: HashSet<(i64, i64)> = HashSet::new();
-        seats.insert((0, 1));
-        seats.insert((0, 0));
-        seats.insert((1, 1));
-        let mut occupied: HashSet<(i64, i64)> = HashSet::new();
-        occupied.insert((0, 0));
-        occupied.insert((0, 1));
-        assert_eq!(adjacent_occupied(&(1, 1), &seats, &occupied, &(3, 3)), 2);
-
-        // #L..
-        // .#.#
-        // ....
-        let mut seats: HashSet<(i64, i64)> = HashSet::new();
-        seats.insert((0, 1));
-        seats.insert((0, 0));
-        seats.insert((1, 1));
-        seats.insert((3, 1));
-        let mut occupied: HashSet<(i64, i64)> = HashSet::new();
-        occupied.insert((0, 0));
-        occupied.insert((3, 1));
-        assert_eq!(adjacent_occupied(&(1, 1), &seats, &occupied, &(4, 3)), 2);
-
-        // #L..
-        // .#.#
-        // ....
-        // ...#
-        let mut seats: HashSet<(i64, i64)> = HashSet::new();
-        seats.insert((0, 1));
-        seats.insert((0, 0));
-        seats.insert((1, 1));
-        seats.insert((3, 1));
-        seats.insert((3, 3));
-        let mut occupied: HashSet<(i64, i64)> = HashSet::new();
-        occupied.insert((0, 0));
-        occupied.insert((3, 1));
-        occupied.insert((3, 3));
-        assert_eq!(adjacent_occupied(&(1, 1), &seats, &occupied, &(4, 4)), 3);
-
-        Ok(())
-    }
 }
